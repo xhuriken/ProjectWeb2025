8c0399315a6b7325df9286258eacde8d5db9fe31
xhuriken <celestin@honvault.Com>
2025-04-09 15:43:27 +0200

    Add cohorts in sidebar !

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use App\Models\Cohort;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        View::share('cohorts', Cohort::all());
         Schema::defaultStringLength(191);
     }
 }
--- resources/views/components/main/sidebar.blade.php
+++ resources/views/components/main/sidebar.blade.php
@@ -59,22 +59,18 @@
                             </a>
                         </div>
                         <!-- @todo CETTE PARTIE EST A GENERER DEPUIS LA BDD -->
-                        <div class="menu-item">
-                            <a class="menu-link py-2 px-2.5 rounded-md border border-transparent menu-item-active:border-gray-200 menu-item-active:bg-light menu-link-hover:bg-light menu-link-hover:border-gray-200"
-                               href="#">
-                               <span class="menu-title text-2sm text-gray-800 menu-item-active:text-gray-900 menu-link-hover:text-gray-900">
-                                B1 - Cergy - 24-25
-                               </span>
-                            </a>
-                        </div>
-                        <div class="menu-item">
-                            <a class="menu-link py-2 px-2.5 rounded-md border border-transparent menu-item-active:border-gray-200 menu-item-active:bg-light menu-link-hover:bg-light menu-link-hover:border-gray-200"
-                               href="#">
-                               <span class="menu-title text-2sm text-gray-800 menu-item-active:text-gray-900 menu-link-hover:text-gray-900">
-                                B2 - Cergy - 24-25
-                               </span>
-                            </a>
-                        </div>
+                        <!-- C'est fait :) -->
+                        @foreach($cohorts as $cohort)
+                            <div class="menu-item">
+                                <a class="menu-link py-2 px-2.5 rounded-md border border-transparent menu-item-active:border-gray-200 menu-item-active:bg-light menu-link-hover:bg-light menu-link-hover:border-gray-200"
+                                   href="{{ route('cohort.show', $cohort->id) }}">
+                                    <span class="menu-title text-2sm text-gray-800 menu-item-active:text-gray-900 menu-link-hover:text-gray-900">
+                                        {{ $cohort->name }} - {{ $cohort->description }} - {{ \Carbon\Carbon::parse($cohort->start_date)->format('y') }}-{{ \Carbon\Carbon::parse($cohort->end_date)->format('y') }}
+
+                                    </span>
+                                </a>
+                            </div>
+                        @endforeach
                         <!-- FIN -->
                    </div>
                 </div>
